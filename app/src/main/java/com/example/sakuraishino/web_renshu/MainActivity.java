1c25b405887557f874e8e70fa3f5b3a878172203
櫻井紫乃 <sakuraishino@matsushitamitsunori-no-MacBook-Pro.local>
2015-03-01 16:35:13 +0900

    second commit

---
+++ app/src/main/java/com/example/sakuraishino/web_renshu/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.sakuraishino.web_renshu;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class MainActivity extends ActionBarActivity {
+
+ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        Picasso.with(this).load("http://pbs.twimg.com/profile_images/3447078122/586b44f30271cd15201ce72065ef2ee2.jpeg").into(imageView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}
